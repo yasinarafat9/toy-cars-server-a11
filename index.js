0199530933ba2ccd340456493438c5929afc09c6
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-20 00:05:29 +0600

    delete button successfully deleted data from database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,18 +27,47 @@ async function run() {
     // post addToy
     const toysCollection = client.db("toysDB").collection("addToy");
 
-    app.post('/addToy', async(req, res)=>{
+
+    app.post('/addToy', async (req, res) => {
       const newAddToy = req.body;
       console.log(newAddToy)
       const result = await toysCollection.insertOne(newAddToy);
       res.send(result)
     })
 
-    app.get('/addToy', async(req, res)=>{
+    app.get('/addToy', async (req, res) => {
       const cursor = toysCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/addToy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    })
+
+
+    //get by email
+    app.get('/addToy', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    //delete
+    app.delete('/addToy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
@@ -53,10 +82,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',(req, res)=> {
-    res.send('Toy Car Server is Doing its Job')
+app.get('/', (req, res) => {
+  res.send('Toy Car Server is Doing its Job')
 })
 
-app.listen(port, () =>{
-    console.log(`Toy Car Server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Toy Car Server is running on port ${port}`)
 })
