bbe14689490d11a411829f0e6292617829481fb6
yasinarafat9 <yasinarafat9612@gmail.com>
2023-05-19 12:38:26 +0600

    post add toy in database

--- index.js
+++ index.js
@@ -24,12 +24,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // post addToy
+    const toysCollection = client.db("toysDB").collection("addToy");
+
+    app.post('/addToy', async(req, res)=>{
+      const newAddToy = req.body;
+      console.log(newAddToy)
+      const result = await toysCollection.insertOne(newAddToy);
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
